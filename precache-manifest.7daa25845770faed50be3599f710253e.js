58c4ea2b47d28f5a6b504c5ae011e459865baccd
macbook <macbook@MBP-macbook.fritz.box>
2020-05-21 15:24:14 +0200

    Updates

---
+++ precache-manifest.7daa25845770faed50be3599f710253e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd59aa3ed532e3db06c9d65b719c17ed",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "06e07906e3775fe04908",
+    "url": "/weather-app/static/css/main.cba9fb66.chunk.css"
+  },
+  {
+    "revision": "a5c272904e63c1321319",
+    "url": "/weather-app/static/js/2.0af171bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.0af171bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e07906e3775fe04908",
+    "url": "/weather-app/static/js/main.54edb33b.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  }
+]);
