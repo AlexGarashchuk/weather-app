17f933e50b0688efd5637de587fccf1573b85e88
macbook <macbook@MBP-macbook.fritz.box>
2020-05-21 15:59:41 +0200

    removed switch component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import WeatherData from "./Components/WeatherData";
 import Form from "./Components/Form";
 import CityList from "./Components/CityList";
 import DetailsPage from "./Components/WeatherDataDetails";
-import './index.css'
+import "./index.css";
 
 function App() {
   return (
@@ -12,15 +12,12 @@ function App() {
       <div className="box">
         <h2>Sinoptik</h2>
         <Form />
-        <CityList/>
-        <Switch>
-          <Route path="/" exact  component={WeatherData}></Route>
-          <Route path="/Berlin" component={DetailsPage} />
-        </Switch>
+        <CityList />
+        <Route path="/" exact component={WeatherData}></Route>
+        <Route path="/Berlin" component={DetailsPage} />
       </div>
     </div>
   );
 }
 
- 
 export default App;
