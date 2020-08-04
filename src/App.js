4811644d41b11f4dd1ae181ea2807c1ca83c4f82
macbook <macbook@MacBook-Pro-macbook.local>
2020-08-04 18:32:58 +0300

    added router for details and basic components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from "react";
-import WeatherData from "./Components/WeatherData";
-import Form from "./Components/Form";
-import CityList from "./Components/CityList";
-import './index.css'
-import { CurrentPlace } from "./Components/CurrentPlace";
+import "./index.css";
+import { Details } from "./Components/Details";
+import { Basic } from "./Components/Basic";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="wrapper">
       <div className="box">
         <h2>Sinoptik</h2>
-        <Form />
-        <CityList/>
-        <CurrentPlace />
-        <WeatherData />
-          {/* <Route path="/" exact  component={}></Route>
-          <Route path="/Berlin" component={DetailsPage} /> */}
-      
+        <Route exact path="/" component={Basic} />
+        <Route path="/details" component={Details} />
       </div>
     </div>
   );
 }
 
- 
 export default App;
